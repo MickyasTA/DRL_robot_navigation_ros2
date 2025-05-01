91ba2572beecce03a9139367e62c344ef7aff3f6
Mickyas Tamiru Asfaw <136974810+MickyasTA@users.noreply.github.com>
2025-05-01 23:22:29 +0200

    Delete build directory

--- build/omni_cartographer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/perla/DRL_robot_navigation_ros2/src/omni_cartographer
-# Build directory: /home/perla/DRL_robot_navigation_ros2/build/omni_cartographer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/velodyne_simulator/CMakeFiles/velodyne_simulator_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/velodyne_simulator_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/velodyne_simulator_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
